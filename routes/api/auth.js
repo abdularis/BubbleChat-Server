ab43efea372bd6439e26c0975c02babca5c2e77b
abdularis <abdularisrahmanudin10@gmail.com>
2019-02-21 11:03:23 +0000

    add verify otp code

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,8 @@ const { OtpRecord } = require('../../models/models');
 
 const router = express.Router();
 
+const OTP_LENGTH = 6;
+
 const signinValidator = [
     check('phone_num').exists()
 ];
@@ -32,7 +34,7 @@ router.post('/signin', signinValidator, (req, res) => {
         if (result) {
             throw new Error("OTP request is already requested.");
         } else {
-            const code = otpGen.generate(6, { alphabets: false, upperCase: false, specialChars: false });
+            const code = otpGen.generate(OTP_LENGTH, { alphabets: false, upperCase: false, specialChars: false });
             return OtpRecord.build({
                 code: code,
                 phoneNumber: phoneNumber,
@@ -50,5 +52,52 @@ router.post('/signin', signinValidator, (req, res) => {
         res.status(400).json({ error: String(err) });
     });
 });
+
+
+const verifyOtpValidator = [
+    check('code').exists().isLength(OTP_LENGTH),
+    check('phone_num').exists()
+]
+
+router.post('/verifycode', verifyOtpValidator, (req, res) => {
+    const phoneNumber = req.body.phone_num;
+    const code = req.body.code;
+
+    OtpRecord.findOne({
+        where: {
+            phoneNumber: phoneNumber,
+            isUsed: false,
+            code: code,
+            expireAt: {
+                [Sequelize.Op.gt]: new Date()
+            }
+        }
+    })
+    .then(result => {
+        console.log(result);
+        if (result) {
+            return OtpRecord.update({
+                isUsed: true
+            }, {
+                where: {
+                    id: result.id
+                }
+            });
+        } else {
+            throw new Error("Failed to verify OTP, expire or invalid code");
+        }
+    })
+    .then(result => {
+        if (result) {
+            res.send({
+                "status": "success"
+            });
+        }
+    })
+    .catch(err => {
+        res.status(400).json({ error: String(err) });
+    });
+});
+
 
 module.exports = router;
