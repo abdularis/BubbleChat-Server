4a248be3dff7c82d4b4183d9fd14f56d36387f94
abdularis <abdularisrahmanudin10@gmail.com>
2019-03-08 08:27:34 +0000

    create dummy signalling

--- routes/websocket/app.js
+++ routes/websocket/app.js
@@ -1,6 +1,25 @@
-function onConnection(socket) {
-    socket.on('/call/offer', function(args) {
-        socket.emit('/call/answer', "Hello from offer, hahahahaha");
+const socketIdByPhone = new Map();
+const phoneBySocketId = new Map();
+
+function onConnection(io, socket) {
+    socket.on('/signalling/send', function(data) {
+        data = JSON.parse(data);
+
+        const phone = data.address;
+        const s = io.sockets.connected[socketIdByPhone[phone]];
+        console.log("to " + data.address + ", cid: " + data.message.call_id);
+        console.log("\tmsid " + socket.id);
+        if (s) {
+            console.log("\tsid " + s.id + ", phone " + phoneBySocketId[s.id]);
+            data.address = phoneBySocketId[socket.id];
+            s.emit('/signalling/receive', data);
+        }
+    });
+
+    socket.on('/storePhoneNumber', function(data) {
+        socketIdByPhone[data] = socket.id;
+        phoneBySocketId[socket.id] = data;
+        console.log("stored: " + data + ", " + socket.id);
     });
 }
 
